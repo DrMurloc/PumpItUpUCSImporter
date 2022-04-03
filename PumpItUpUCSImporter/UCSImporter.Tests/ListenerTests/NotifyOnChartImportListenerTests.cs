d1f9b4a684715c4f67ff7c0a956f389b52c26e1d
Jonathan Eccker <joneccker@gmail.com>
2022-04-03 15:39:15 -0400

    Added message pushing for imported charts

---
+++ PumpItUpUCSImporter/UCSImporter.Tests/ListenerTests/NotifyOnChartImportListenerTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using FakeItEasy;
+using UCSImporter.Application.Events;
+using UCSImporter.Application.Listeners;
+using UCSImporter.Domain.Contracts;
+using UCSImporter.Domain.Models;
+using UCSImporter.Domain.ValueTypes;
+using UCSImporter.Tests.Helpers;
+using Xunit;
+
+namespace UCSImporter.Tests.ListenerTests;
+
+public sealed class NotifyOnChartImportListenerTests
+{
+    private readonly Fixture _fixture = FixtureBuilder.Build();
+
+    [Fact]
+    public async Task ChartToMessageMappingTest()
+    {
+        //Test Data
+        var chart = _fixture.Create<Chart>();
+        var chart2 = _fixture.Create<Chart>();
+
+        var expected = $@"
+{chart.Song.Name}: {chart.Type} {chart.Level} by {chart.Artist.Name} created on {chart.CreationDate}
+{chart2.Song.Name}: {chart2.Type} {chart2.Level} by {chart2.Artist.Name} created on {chart2.CreationDate}";
+
+        //Set Up
+        var client = A.Fake<IMessageClient>();
+
+        var listener = new NotifyOnChartImportListener(client);
+
+        //Test
+
+        await listener.Handle(new ChartsImportedEvent(new[] { chart, chart2 }), CancellationToken.None);
+
+        //Assert
+        A.CallTo(() => client.SendMessage(A<Message>.That.Matches(m => m == expected), A<CancellationToken>.Ignored))
+            .MustHaveHappenedOnceExactly();
+    }
+}
---
+++ PumpItUpUCSImporter/UCSImporter.Tests/ModelTests/ChartTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AutoFixture;
+using UCSImporter.Domain.Enums;
+using UCSImporter.Domain.Models;
+using UCSImporter.Domain.ValueTypes;
+using UCSImporter.Tests.Helpers;
+using Xunit;
+
+namespace UCSImporter.Tests.ModelTests;
+
+public sealed class ChartTests
+{
+    private readonly Fixture _fixture = FixtureBuilder.Build();
+
+    [Theory]
+    [InlineData(5)]
+    [InlineData(1)]
+    [InlineData(100000)]
+    public void ChartsWithSameIdAreEqual(int chartId)
+    {
+        var chart1 = new Chart(chartId, _fixture.Create<Song>(),
+            _fixture.Create<ChartType>(),
+            _fixture.Create<DifficultyLevel>(),
+            _fixture.Create<StepArtist>(), _fixture.Create<DateOnly>());
+        var chart2 = new Chart(chartId, _fixture.Create<Song>(),
+            _fixture.Create<ChartType>(),
+            _fixture.Create<DifficultyLevel>(),
+            _fixture.Create<StepArtist>(), _fixture.Create<DateOnly>());
+
+        Assert.Equal(chart1, chart2);
+        Assert.True(chart2.Equals((object)chart1));
+        Assert.True(chart1.Equals((object)chart2));
+        Assert.Equal(chart1.GetHashCode(), chart2.GetHashCode());
+    }
+}
