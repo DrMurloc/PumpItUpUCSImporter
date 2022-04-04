ee129e4b50479a25b38ecc3b465a166ec1d2fe75
Jonathan Eccker <joneccker@gmail.com>
2022-04-04 18:09:48 -0400

    Added discord bot interaction for notifying on chart imports

---
+++ PumpItUpUCSImporter/UCSImporter.Data/DiscordMessageClient.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using UCSImporter.Data.Configuration;
+using UCSImporter.Domain.Contracts;
+using UCSImporter.Domain.ValueTypes;
+
+namespace UCSImporter.Data;
+
+public sealed class DiscordMessageClient : IMessageClient
+{
+    private readonly DiscordConfiguration _config;
+    private readonly ILogger _logger;
+
+    public DiscordMessageClient(ILogger<DiscordMessageClient> logger,
+        IOptions<DiscordConfiguration> discordConfiguration)
+    {
+        _logger = logger;
+        _config = discordConfiguration.Value;
+    }
+
+    public async Task SendMessage(Message message, CancellationToken cancellationToken = default)
+    {
+        var client = new DiscordSocketClient(new DiscordSocketConfig
+        {
+            LogLevel = LogSeverity.Verbose
+        });
+        client.Log += msg =>
+        {
+            _logger.LogInformation(msg.Message);
+            return Task.CompletedTask;
+        };
+        await client.LoginAsync(TokenType.Bot, _config.BotToken);
+        await client.StartAsync();
+
+        foreach (var channelId in _config.ChannelIds)
+        {
+            var channel = await client.GetChannelAsync(channelId) as IMessageChannel;
+            if (channel == null)
+            {
+                _logger.LogWarning($"Channel {channel} was not reachable");
+                continue;
+            }
+
+            await channel.SendMessageAsync(message);
+        }
+
+        await client.StopAsync();
+    }
+}
---
+++ PumpItUpUCSImporter/UCSImporter.Data/InMemory/InMemoryNewChartRepository.cs
@@ -0,0 +1,18 @@
+﻿using UCSImporter.Domain.Contracts;
+using UCSImporter.Domain.Enums;
+using UCSImporter.Domain.Models;
+
+namespace UCSImporter.Data.InMemory;
+
+public sealed class InMemoryNewChartRepository : INewChartRepository
+{
+    public Task<IEnumerable<Chart>> GetNewCharts(int page, CancellationToken cancellationToken = default)
+    {
+        if (page > 1) return Task.FromResult<IEnumerable<Chart>>(Array.Empty<Chart>());
+        return Task.FromResult<IEnumerable<Chart>>(new[]
+        {
+            new Chart(1, new Song("Song"), ChartType.Double, 14, new StepArtist("Artist"),
+                DateOnly.FromDateTime(DateTime.Now))
+        });
+    }
+}
